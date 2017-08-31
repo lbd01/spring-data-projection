1f1a371cf704a9cde7de38c3d7ec79cde1694b96
lbd01 <piotr.stepaniak@gmail.com>
2017-08-31 19:28:17 +0200

    Move data init to script

--- src/test/java/stackoverflow/spring/data/PersonRepositoryTest.java
+++ src/test/java/stackoverflow/spring/data/PersonRepositoryTest.java
@@ -20,16 +20,8 @@ public class PersonRepositoryTest {
 
     @Test
     public void test() {
-        Person p = new Person(null,"John","Wayne");
-        pRepo.save(p);
-        ContactInfo c1 = new ContactInfo(null, p.getId(),"+1 123 123 123");
-        cRepo.save(c1);
-        ContactInfo c2 = new ContactInfo(null, p.getId(),"john.wayne@west.com");
-        cRepo.save(c2);
-
         List<Person> list = pRepo.findAll();  //select generatedAlias0 from Person as generatedAlias0
         List<PersonProjection> listProjection = pRepo.findAllProjectedBy();  //select contactInfoList, generatedAlias0.id from Person as generatedAlias0 left join generatedAlias0.contactInfoList as contactInfoList
-
         Assert.assertTrue(list.size()==listProjection.size());
     }
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,6 @@
+insert into person (id,name,lastname) values
+(1,'John','Wayne');
+
+insert into contact_info (id, person_id, description) values
+(1,1,'+1 123 123 123'),
+(2,1,'john.wayne@west.com');
